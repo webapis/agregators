f6b095e1744e0cde92ffc8ab060f18b7c31fb700
webapis <webapis.github@gmail.com>
2021-03-19 09:07:17 +0500

    v

--- page-content-reader/index.js
+++ page-content-reader/index.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
 async function pageContentReader({ url }) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2' });
-  const pageTitle = await page.title();
-  return Promise.resolve(pageTitle);
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const pageTitle = await page.title();
+    await page.close();
+    await browser.close();
+    return Promise.resolve(pageTitle);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
 module.exports = {
