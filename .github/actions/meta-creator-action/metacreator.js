4836850fb10f996af9cff6b0eb1614d89d231f3c
webapis <webapis.github@gmail.com>
2021-03-20 10:41:12 +0500

    v

--- .github/actions/meta-creator-action/metacreator.js
+++ .github/actions/meta-creator-action/metacreator.js
@@ -6,8 +6,8 @@ const {
 try {
   const pageData = core.getInput('pageData');
   console.log('pageData length inside creator action', pageData.length);
-  const data = defactoKadynJeanMetaData(pageData).then(pageMete => {
-    core.setOutput('pageMeta', data['pantolonPage']);
+  // const data = defactoKadynJeanMetaData(pageData).then(pageMete => {
+  //   core.setOutput('pageMeta', data['pantolonPage']);
   });
 } catch (error) {
   core.setFailed(error.message);
