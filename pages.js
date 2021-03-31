746777ac4e020327b73d4dd7fe283603f20a517b
webapis <webapis.github@gmail.com>
2021-03-31 15:23:20 +0500

    v

--- pages.js
+++ pages.js
@@ -4,37 +4,36 @@ const {
 const {
   defactoNextPageUrls
 } = require('./page-data-collector/defacto/defactoNextPageUrls');
-// const {
-//   defactoKadynJeanMetaData
-// } = require('./page-meta-creator/defacto/kadin/jean/defacto.kadyn.jean.metadata');
 
 const pages = [
   {
-    pageName: '/index.html',
-    input: 'src/index.js',
-    selector: '#root',
-    component: 'div',
-    id: 'root',
-    jsonUrl: '',
-    pageDataCollector: null,
-    pageMetaCreator: null
+    pageData: null,
+    pageMeta: null,
+    pageBuild: {
+      pageName: '/index.html',
+      input: 'src/index.js',
+      component: 'div',
+      id: 'root',
+      jsonUrl: ''
+    },
+    pagePrerender: { selector: '#root' }
   },
   {
-    pageName: '/defacto/kadin/jean/pantolon.html',
-    input: 'src/product-list-page.js',
-    selector: 'product-list',
-    component: 'product-list',
-    id: 'defacto-kadin-jean-pantolon',
-    jsonUrl: './defacto-kadin-jean-pantolon.json',
-    dataCollection: {
+    pageData: {
       sourceUrl: 'https://www.defacto.com.tr/kadin-denim',
       dataCollector: defactoDataCollector,
       pageUrlsGetter: defactoNextPageUrls,
       output: `${process.cwd()}/page-data/defacto/kadin/kadin-jeans.json`
     },
-    metaCreation: {
-     // metaCreator: defactoKadynJeanMetaData
-    }
+    pageMeta: {},
+    pageBuild: {
+      pageName: '/defacto/kadin/jean/pantolon.html',
+      input: 'src/product-list-page.js',
+      component: 'product-list',
+      id: 'defacto-kadin-jean-pantolon',
+      jsonUrl: './defacto-kadin-jean-pantolon.json'
+    },
+    pagePrerender: { selector: 'product-list' }
   }
 ];
 
