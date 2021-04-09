15314b6657edb200635937357d496525a29db32e
webapis <webapis.github@gmail.com>
2021-04-09 15:25:59 +0500

    v

--- page-data-collector/server.js
+++ page-data-collector/server.js
@@ -1,10 +1,14 @@
-const { pages } = require('../pages');
 const { pageDataCollector } = require('./pageDataCollector');
-
-pages.filter(p => p.pageData !== null).map(async p => {
-  const {
-    pageData: { input, dataCollectorFunc, pageUrlsGetterFunc, output }
-  } = p;
+const pages = [
+  {
+    input: 'https://www.defacto.com.tr/kadin-denim',
+    output: `${process.cwd()}/page-data/defacto/tr/kadin-denim.json`,
+    dataCollectorFunc: './defacto/tr/dataCollector.js',
+    pageUrlsGetterFunc: './defacto/tr/pageUrlsGetter.js'
+  }
+];
+pages.map(async p => {
+  const { input, dataCollectorFunc, pageUrlsGetterFunc, output } = p;
   const { dataCollector } = require(dataCollectorFunc);
   const { pageUrlsGetter } = require(pageUrlsGetterFunc);
   await pageDataCollector({
