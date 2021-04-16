44a43092b4144cd85c97eadb893324fc4ebce164
webapis <webapis.github@gmail.com>
2021-04-16 17:00:13 +0500

    v

--- page-prerender/pages.js
+++ page-prerender/pages.js
@@ -1,11 +1,7 @@
 const pages = [
   {
     input: `${process.cwd()}/page-meta/tr/moda/defacto/kadin/jean/jean-kategoriler.json`,
-<<<<<<< HEAD
     pagePrerenderFunc: `${process.cwd()}/page-prerender/tr/moda/defacto/jeans/pagePrerender.js`,
-=======
-    pagePrerenderFunc: `${process.cwd()}/page-prerender/defacto/jeans/pagePrerender.js`,
->>>>>>> koton
     selector: 'product-list'
   }
 ];
