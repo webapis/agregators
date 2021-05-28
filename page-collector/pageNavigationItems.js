0e9db9d48e7990ff88f5c3ea06cdd9bb5b7fe4b0
webapis <webapis.github@gmail.com>
2021-05-28 12:10:00 +0500

    v

--- page-collector/pageNavigationItems.js
+++ page-collector/pageNavigationItems.js
@@ -31,12 +31,13 @@ function pageNavigationItems() {
       if (!element) {
         let element = document.createElement('ul');
 
-        let linkFromParent = `/` + parentId.replace(/-/g, '/') + '.html';
-
+        let linkFromParent = `/tr/` + parentId.replace(/-/g, '/') + '.html';
+        debugger;
         if (!document.querySelector(`[href='${linkFromParent}']`)) {
           let linkElementParent = document.createElement('li');
           let a = document.createElement('a');
           a.href = linkFromParent;
+
           a.textContent = 'tüm ' + path.basename(linkFromParent, '.html');
           linkElementParent.appendChild(a);
 
@@ -46,9 +47,12 @@ function pageNavigationItems() {
         let span = document.createElement('li');
         span.textContent = p;
         span.classList.add('arrow');
+
+        span.setAttribute('aria-expanded', 'false');
         span.setAttribute('data-bs-toggle', 'collapse');
         span.setAttribute('data-bs-target', id);
         parentElement.appendChild(span);
+
         element.id = id;
         element.classList.add('collapse');
         parentElement.appendChild(element);
@@ -100,196 +104,12 @@ function pageNavigationItems() {
   debugger;
 
   console.log('page nav item collection ended...');
-}
+} 
 
 function getPosition(string, subString, index) {
   return string.split(subString, index).join(subString).length;
 }
 
-function comId(str) {
-  if (str && str !== '' && str.includes('/')) {
-    const replaced = str.replace(/\//, '-');
-    debugger;
-    return replaced;
-  }
-  return str;
-}
-
 module.exports = {
   pageNavigationItems
 };
-
-/*
-const { walkSync } = require('./walkSync');
-const fs = require('fs');
-const path = require('path');
-const makeDir = require('make-dir');
-const { JSDOM } = require('jsdom');
-function pageNavigationItems() {
-  let files = [];
-
-  console.log('page nav item collection started....');
-
-  const dir = `${process.cwd()}/page-list-pages/`;
-
-  walkSync(dir, filepath => {
-    files.push(filepath.substring(filepath.indexOf('moda')));
-  });
-  const dom = new JSDOM(``);
-  const { document } = dom.window;
-  //--------
-  let i = 1;
-  while (i < 6) {
-    let mapped = files.map(f =>
-      f.substring(0, getPosition(f, '/', i)).replace('.html', '')
-    );
-
-    if (i === 1) {
-      mapped.forEach(m => {
-        let id = m;
-        let element = document.getElementById(id);
-        if (!element) {
-          let element = document.createElement('ul');
-          element.id = id;
-
-          document.body.appendChild(element);
-        }
-      });
-    } else {
-      mapped.forEach(m => {
-        let id = m;
-        const parentId = m.substring(0, getPosition(m, '/', i - 1));
-        let parentElement = document.getElementById(parentId);
-
-        let element = document.getElementById(id);
-
-        if (!element) {
-          if (i === 5) {
-            let element = document.createElement('li');
-            element.id = id;
-            parentElement.appendChild(element);
-          } else {
-            let element = document.createElement('ul');
-
-            let container = document.createElement('div');
-            container.classList.add('collapse');
-
-            container.id = id;
-            element.appendChild(container);
-
-            // element.classList.add('collapse');
-
-            parentElement.appendChild(element);
-          }
-        }
-      });
-    }
-
-    i++;
-  }
-
-  files.forEach(f => {
-    const id = f.replace('.html', '');
-    const moreThanOne = files.filter(f => f.includes(id)).length;
-    const element = document.getElementById(id);
-    const textContent = path.basename(f, '.html').replace('-', ' ');
-    if (moreThanOne > 1) {
-      const a = document.createElement('a');
-      a.href = `/tr/${f}`;
-      a.textContent = `tümü (${textContent})`;
-      let li = document.createElement('li');
-      li.appendChild(a);
-      let child = document.createElement('div');
-      child.textContent = 'child';
-      element.prepend(li);
-      li.nextSibling.prepend(li.cloneNode(true));
-      li.innerHTML = '';
-      const spn = document.createElement('li');
-      //spn.setAttribute('data-bs-toggle', 'collapse');
-      spn.setAttribute('data-bs-target', id);
-      spn.setAttribute('aria-expanded', true);
-      spn.setAttribute('role', 'button');
-      spn.textContent = textContent;
-      spn.classList.add('arrow');
-      //spn.classList.add('btn');
-      element.parentNode.prepend(spn);
-    } else {
-      const a = document.createElement('a');
-      a.href = `/tr/${f}`;
-      a.textContent = textContent;
-      element.prepend(a);
-    }
-  });
-  document.querySelectorAll('*').forEach(node => {
-    const str = node.id;
-    if (str && str !== '' && str.includes('/')) {
-      const replaced = str.replace(/\//g, '-');
-
-      node.setAttribute('id', replaced);
-    }
-  });
-  document.querySelectorAll('*').forEach(node => {
-    const str = node.getAttribute('data-bs-target');
-    if (str && str !== '' && str.includes('/')) {
-      const replaced = str.replace(/\//g, '-');
-
-      node.setAttribute('data-bs-target', replaced);
-    }
-  });
-
-  document.querySelectorAll('ul[data-bs-target]').forEach(node => {
-    debugger;
-    // let lic = document.createElement('ul');
-    // lic.textContent = 'child';
-    const parentElement = node.parentElement;
-    const tagName = parentElement.tagName;
-    let lic = parentElement.previousSibling;
-
-    //if (tagName === 'UL') {
-    node.parentNode.prepend(lic);
-
-    debugger;
-    //collapse.insertBefore(li.cloneNode(true), collapse);
-
-    debugger;
-  });
-  // document.querySelectorAll('ul[id]').forEach(node => {
-  //   let li = node.firstChild;
-
-  //   li.innerHTML = '';
-  //   debugger;
-  // });
-  document.querySelectorAll('li').forEach(node => {
-    if (node.innerHTML === '') {
-     node.parentNode.removeChild(node)
-    }
-    debugger;
-  });
-  // --------
-  const content = `<div class="side-nav">${document.body.innerHTML}</div>`;
-  const dirName = `${process.cwd()}/page-navigation`;
-  makeDir.sync(dirName);
-  fs.writeFileSync(`${dirName}/nav.html`, content);
-  debugger;
-
-  console.log('page nav item collection ended...');
-}
-
-function getPosition(string, subString, index) {
-  return string.split(subString, index).join(subString).length;
-}
-
-function comId(str) {
-  if (str && str !== '' && str.includes('/')) {
-    const replaced = str.replace(/\//, '-');
-    debugger;
-    return replaced;
-  }
-  return str;
-}
-
-module.exports = {
-  pageNavigationItems
-};
-
-*/
--- src/csr-components/nav-component.js
+++ src/csr-components/nav-component.js
@@ -27,23 +27,32 @@ customElements.define(
             } else {
               id += `-${p}`;
 
-              if (i !== 0) {
+              //if (i !== 0) {
+                debugger;
                 document.getElementById(id).classList.toggle('collapse');
+                // const arrow =document.querySelector(`li[data-bs-target=${id}]`)
+                // //const isExpaded = arrow.getAttribute('aria-expanded');
+                // arrow.setAttribute('aria-expanded', 'true');
+                //debugger;
                 console.log('id', id);
-              }
+              //}
             }
+            document.getElementById(id).classList.toggle('collapse');
           });
 
-          document.querySelectorAll('[data-bs-target]').forEach(element => {
+          document.querySelectorAll('.arrow').forEach(element => {
+            
+
             element.addEventListener('click', function() {
               let id = element.getAttribute('data-bs-target');
               const expander = document.getElementById(id);
-              const isExpaded = Boolean(element.getAttribute('aria-expanded'));
+              const isExpaded = element.getAttribute('aria-expanded');
+
               console.log('isExpaded', isExpaded);
-              if (isExpaded) {
-                element.setAttribute('aria-expanded', false);
+              if (isExpaded === 'true') {
+                element.setAttribute('aria-expanded', 'false');
               } else {
-                element.setAttribute('aria-expanded', true);
+                element.setAttribute('aria-expanded', 'true');
               }
 
               expander.classList.toggle('collapse');
