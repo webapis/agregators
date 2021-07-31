2bcfbfdb33f8b71b240ba5ccea4b1d262b1a9869
webapis <webapis.github@gmail.com>
2021-07-31 11:30:28 +0500

    v

--- utils/crawler/puppeteerCrawler.js
+++ utils/crawler/puppeteerCrawler.js
@@ -7,7 +7,7 @@ const { URL } = require('url')
 async function puppeteerCrawler({ handlePageFunction, headless, preNavHook, postNavHook, complete }) {
  
         let eventEmitter = promiseConcurrency({
-            batchConcurrency: 20, rejectedRetry: 3,
+            batchConcurrency: 5, rejectedRetry: 3,
             taskName: 'dataCollection'
         });
         global.pc_eventEmitter = eventEmitter
