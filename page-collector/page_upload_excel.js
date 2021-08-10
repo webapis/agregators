11bc48b1d67e76b05baf8341e77aa7a62af424f6
webapis <webapis.github@gmail.com>
2021-08-10 09:23:23 +0500

    v

--- page-collector/page_upload_excel.js
+++ page-collector/page_upload_excel.js
@@ -1,8 +1,14 @@
 const { fbDatabase } = require('../utils/firebase/firebaseInit')
+const { Blob } = require('buffer')
 const { walkSync } = require('./walkSync');
 const { refreshAccessToken } = require('../index')
 const fs = require('fs')
+const path = require('path')
+
+
 const fetch = require('node-fetch')
+
+
 async function pageUploadExcel({ taskSequelizerEventEmitter }) {
     const email = process.env.email;
     const projectName = process.env.projectName
@@ -32,20 +38,41 @@ async function pageUploadExcel({ taskSequelizerEventEmitter }) {
             try {
                 const response = await uploadFile({ access_token, files })
                 debugger;
-                // const contentType = response.headers.get('content-type')
-                const { error } = await response.json()
-                if (error.code = 401) {
+
+                const contentType = response.headers.get('content-type')
+
+
+                debugger;
+                const status = response.status
+                debugger;
+                if (status === 401) {
+                    debugger;
                     const { access_token } = await refreshAccessToken({
                         refresh_token, email, userkey, cb: async () => {
+                            debugger;
                             const response = await uploadFile({ access_token, files })
+                            debugger;
+                            if (response.ok) {
+                                debugger;
+                                taskSequelizerEventEmitter.emit('taskComplete', 'page_upload_excel')
+                            } else {
+                                taskSequelizerEventEmitter.emit('taskFailed', 'page_upload_excel')
+                            }
                             debugger;
                         }
                     })
                     //const response = await uploadFile({ access_token })
                     debugger;
+                } else if (status === 400) {
+                    debugger;
+                    const contentType = response.headers.get('content-type')
+                    debugger;
+                    const error = await response.text()
+                    debugger;
                 }
                 debugger;
             } catch (error) {
+                console.log('error', error)
                 debugger;
             }
             debugger;
@@ -57,17 +84,130 @@ async function pageUploadExcel({ taskSequelizerEventEmitter }) {
 }
 
 async function uploadFile({ access_token, files }) {
-    //HEALP FROM: https://stackoverflow.com/questions/44021538/how-to-send-a-file-in-request-node-fetch-or-node
- 
-    const stats = fs.statSync(files[0]);
-    const fileSizeInBytes = stats.size;
-    let readStream = fs.createReadStream(files[0]);
 
-debugger;
-    const apiendpoint = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
-    return await fetch(apiendpoint, {
-        method: 'post', body: readStream,
-        headers: { 'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8', 'Content-Length': fileSizeInBytes, 'Authorization': `Bearer ${access_token}` },
-    })
+    //HEALP FROM: https://stackoverflow.com/questions/44021538/how-to-send-a-file-in-request-node-fetch-or-node
+    try {
+
+        debugger;
+
+        const stats = fs.statSync(files[0]);
+
+        const fileData = fs.readFileSync(files[0])
+
+        const fileSizeInBytes = stats.size;
+        const fileName = path.basename(files[0])
+        const metadata = { name: fileName, mymeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }
+        const body = buildMultipartRelatedBody({ fileData, metadata })
+        const bodySize = fileData.byteLength
+        debugger;
+        const apiendpoint = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart'
+        return await fetch(apiendpoint, {
+            method: 'post',
+            headers: { 'Content-Type': 'multipart/related; boundary=boudaryWord', 'Authorization': `Bearer ${access_token}` },
+            body
+        })
+
+    } catch (error) {
+        debugger;
+    }
+}
+
+function buildMultipartRelatedBody({ metadata, fileData}) {
+    debugger;
+    const { mymeType } = metadata
+    debugger;
+    
+
+    const fileSizeInBytes = fileData.length//stats.size;
+    const boundary = 'boudaryWord'
+    const delimiter = "\r\n--" + boundary + "\r\n"
+    const close_delim = "\r\n--" + boundary + "--"
+    const body = delimiter +
+        'Content-Type:application/json; charset=UTF-8\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type:' + mymeType + '\r\n\r\n' +
+        fileData + '\r\n' +
+        close_delim
+
+    console.log('body', body)
+    debugger;
+    return Buffer.concat([Buffer.from(delimiter +
+        'Content-Type:application/json; charset=UTF-8\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type:' + mymeType + '\r\n\r\n', 'utf-8'), Buffer.from(fileData, 'utf-8'), Buffer.from(JSON.stringify(metadata) +
+            delimiter, 'utf-8')])
+
+
+
 }
 module.exports = { pageUploadExcel }
+
+
+
+/*
+function buildMultipartRelatedBody() {
+
+    const fileName = 'mychat123'
+    const fileData = 'this is a sample data'
+    const contentType = 'text/plain'
+    const metadata = {
+        name: fileName, mymeType: contentType
+    }
+
+    const boundary = 'boudaryWord'
+    const delimiter = "\r\n--" + boundary+"\r\n"
+    const close_delim = "\r\n--" + boundary + "--"
+    const body = delimiter+
+        'Content-Type:application/json; charset=UTF-8\r\n\r\n'+
+        JSON.stringify(metadata)+
+        delimiter+
+        'Content-Type:'+contentType+'\r\n\r\n'+
+        fileData+'\r\n'+
+        close_delim
+
+    console.log('body', body)
+    debugger;
+    return body
+
+
+
+}
+*/
+
+
+/*
+
+
+function buildMultipartRelatedBody({ metadata, fileData }) {
+    debugger;
+    const { mymeType } = metadata
+    debugger;
+    // const fileName = 'mychat123'
+    // const fileData = 'this is a sample data'
+    // const contentType = 'text/plain'
+    // const metadata = {
+    //     name: fileName, mymeType: contentType
+    // }
+
+    const fileSizeInBytes = fileData.length//stats.size;
+    const boundary = 'boudaryWord'
+    const delimiter = "\r\n--" + boundary+"\r\n"
+    const close_delim = "\r\n--" + boundary+"--"
+    const body = delimiter+
+        'Content-Type:application/json; charset=UTF-8\r\n\r\n'+
+        JSON.stringify(metadata)+
+        delimiter +
+        'Content-Type:'+mymeType+'\r\n\r\n' +
+        fileData + '\r\n' +
+        close_delim
+
+    console.log('body', body)
+    debugger;
+    return body
+
+
+
+}
+*/
