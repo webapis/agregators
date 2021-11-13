83fd343b5d5efed4c2791adc7ea1340b8376551a
webapis <webapis.github@gmail.com>
2021-11-13 11:24:54 +0500

    v

--- client/netlify/functions/auth-callback.js
+++ client/netlify/functions/auth-callback.js
@@ -7,8 +7,8 @@ exports.handler = async(event, context) => {
     // /* state helps mitigate CSRF attacks & Restore the previous state of your app */
     // const state = event.queryStringParameters.state
      const response = await fetchGithubAccessToken({ code: code, client_id: process.env.gh_client_id, client_secret: process.env.gh_client_secret })
-
+    const {access_token} =await response.json()
    //  await signInWithIdp({ access_token,filepath:dirPath + 'login.html',key:'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA',res })
-  return {statusCode:200, body:JSON.stringify({greet:"hello ouath2",response})}
+  return {statusCode:200, body:JSON.stringify({greet:"hello ouath2",access_token})}
 
 }
