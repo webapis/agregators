59d3a38b58970edd23786640ba17215d3eceed3a
webapis <webapis.github@gmail.com>
2021-11-15 12:26:44 +0500

    v

--- client/netlify/functions/auth-callback.js
+++ client/netlify/functions/auth-callback.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
   const { access_token } = JSON.parse(response)
   const firebaseauthResponse = await authWithFirebase({ access_token, key: 'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA' })
   const firebaseAuthData = JSON.parse(firebaseauthResponse)
-  //const { localId, idToken,screenName } = firebaseAuthData
+  const { localId, idToken,screenName,federatedId,email,emailVerified,kind,needConfirmation,providerId,oauthAccessToken,photoUrl,refreshToken,expiresIn } = firebaseAuthData
 
   const updatedUserCred =await updateUserCredentials(firebaseAuthData)
 
@@ -18,6 +18,56 @@ exports.handler = async (event, context) => {
   console.log('updatedUserCred??????_________', updatedUserCred)
 
   //  await signInWithIdp({ access_token,filepath:dirPath + 'login.html',key:'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA',res })
-  return { statusCode: 200, body: JSON.stringify({ greet: "hello ouath2", access_token, firebaseAuthData }) }
+  return { statusCode: 200, body: `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+      <link href="./ws-dashboard/breadcrumb.css" rel="stylesheet">
+  </head>
+  
+  <body>
+     
+     
+   
+    
+     
+  
+      <div class="container mt-1">
+  
+          <div class="breadcrumb flat">
+              <a href="/index.html" >Home</a>
+          
+              <a href="/login.html" class="active" id="ws-breadcrumb">Sign in</a>
+              </div>
+      </div>
+  <div class="container">
+      
+      <login-page class="row"></login-page>
+  </div>
+  <input type="hidden" id="federatedId" value=${federatedId}/>
+  <input type="hidden" id="email" value=${email}/>
+  <input type="hidden" id="emailVerified" value=${emailVerified}/>
+  <input type="hidden" id="kind" value=${kind}/>
+  <input type="hidden" id="localId" value=${localId}/>
+  <input type="hidden" id="needConfirmation" value=${needConfirmation}/>
+  <input type="hidden" id="oauthAccessToken" value=${oauthAccessToken}/>
+  <input type="hidden" id="photoUrl" value=${photoUrl}/>
+  <input type="hidden" id="providerId" value=${providerId}/>
+  <input type="hidden" id="screenName" value=${screenName}/>
+  <input type="hidden" id="idToken" value=${idToken}/>
+  <input type="hidden" id="refreshToken" value=${refreshToken}/>
+  <input type="hidden" id="expiresIn" value=${expiresIn}/>
+
+      <script src="./login-page.js"></script>
+   
+  </body>
+  
+  </html>` }
 
 }
