b5beca45d905dec4a09cf4a293a4eb9b3e249f28
webapis <webapis.github@gmail.com>
2021-11-15 10:05:03 +0500

    v

--- client/netlify/functions/auth-callback.js
+++ client/netlify/functions/auth-callback.js
@@ -10,7 +10,7 @@ exports.handler = async(event, context) => {
     const {access_token} = JSON.parse(response)
     const firebaseauthResponse = await authWithFirebase({access_token,key:'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA'})
     const firebaseAuthData =JSON.parse(firebaseauthResponse)
-    await userIsNew(...firebaseAuthData)
+    await userIsNew(firebaseAuthData)
     console.log('firebaseauthResponse.....',firebaseAuthData)
  
    //  await signInWithIdp({ access_token,filepath:dirPath + 'login.html',key:'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA',res })
