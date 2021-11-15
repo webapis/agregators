072a98a721537a14384f08686ef300c3215cfcc9
webapis <webapis.github@gmail.com>
2021-11-15 11:42:13 +0500

    v

--- root/utils/github/index.js
+++ root/utils/github/index.js
@@ -124,11 +124,12 @@ async function updateWorkflowRunner({ access_token, userisOld, screenName }) {
     // const privateData = { token: oauthAccessToken, refreshToken, idToken, screenName, email }
 
     if (userisOld) {
-
-        return await nodeFetch({ host: 'api.github.com', path: `/repos/${screenName}/workflow_runner/merge-upstream`, method: 'post', headers: {'User-Agent':'node-js',  'Authorization': `token ${access_token}`, 'Accept': 'application/vnd.github.v3+json' }, body: JSON.stringify({ branch: 'main' }) })
+        console.log('---1----')
+        return await nodeFetch({ host: 'api.github.com', path: `/repos/${screenName}/workflow_runner/merge-upstream`, method: 'post', headers: {'User-Agent':'node.js',  'Authorization': `token ${access_token}`, 'Accept': 'application/vnd.github.v3+json' }, body: JSON.stringify({ branch: 'main' }) })
 
     } else {
-        return await nodeFetch({ host: 'api.github.com', path: '/repos/webapis/workflow_runner/forks', method: 'post', headers: {'User-Agent':'node-js', 'Authorization': `token ${access_token}`, 'Accept': 'application/vnd.github.v3+json' }})
+        console.log('---2----')
+        return await nodeFetch({ host: 'api.github.com', path: '/repos/webapis/workflow_runner/forks', method: 'post', headers: {'User-Agent':'node.js', 'Authorization': `token ${access_token}`, 'Accept': 'application/vnd.github.v3+json' }})
 
        
     }
