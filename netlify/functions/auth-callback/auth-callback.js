50371fe408490985464f763e3c390ecefa4592d3
webapis <webapis.github@gmail.com>
2021-11-18 08:22:01 +0500

    v

--- netlify/functions/auth-callback/auth-callback.js
+++ netlify/functions/auth-callback/auth-callback.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
   const response = await fetchGithubAccessToken({ code: code, client_id: process.env.gh_client_id, client_secret: process.env.gh_client_secret })
   const { access_token } = JSON.parse(response)
   console.log('access_token____',access_token)
-  const firebaseauthResponse = await authWithFirebase({ access_token, key: 'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA' })
+  const firebaseauthResponse = await authWithFirebase({ access_token, key: process.env.webapikey })
   console.log('firebaseauthResponse____!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',firebaseauthResponse)
   const firebaseAuthData = JSON.parse(firebaseauthResponse)
   const { localId, idToken, screenName, federatedId, email, emailVerified, kind, needConfirmation, providerId, oauthAccessToken, photoUrl, refreshToken, expiresIn } = firebaseAuthData
