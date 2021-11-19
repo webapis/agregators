c682e0dee32a0226a7da9518505c3d6960686803
webapis <webapis.github@gmail.com>
2021-11-19 09:02:28 +0500

    n

--- client/resources.js
+++ client/resources.js
@@ -12,7 +12,7 @@ export default async () => {
         loadedResources[1].initState,
         'page-store'
     );
-    window.projectUrl =window.location.hostname==='localhost'?'/api/fb-dev':'https://workflow-prod-799d4-default-rtdb.firebaseio.com'
+    window.projectUrl =window.location.hostname==='localhost'?'http://localhost:9000':'https://workflow-prod-799d4-default-rtdb.firebaseio.com'
     window.webapikey=window.location.hostname==='localhost'?'AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA':'AIzaSyCM7LDPIq6eelnMH_A8SARtIBDT5Zi5tK8'
     window.actionTypes = loadedResources[1].actionTypes;
 
