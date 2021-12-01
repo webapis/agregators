837a3831f719d2b5741ea2e269b34fecd48fd80d
webapis <webapis.github@gmail.com>
2021-12-01 08:47:20 +0500

    d

--- decryptData.js
+++ decryptData.js
@@ -3,6 +3,42 @@ const path = require('path');
 const makeDir = require('make-dir');
 const fs = require('fs')
 const { walkSync } = require('./root/utils/nodejs/walkSync');
+
+// const data = fs.readFileSync(`${process.cwd()}/mock-data/firebaseAuthData.json`, 'utf8')
+
+let files = [`${process.cwd()}/.env.B64`]
+
+const dirpath = `${process.cwd()}/mock-data`
+
+walkSync(dirpath, function (filepath) {
+    if (path.extname(filepath) === '.B64') {
+        files.push(filepath)
+    }
+
+
+})
+
+for (let filepath of files) {
+
+    const data = fs.readFileSync(filepath,{encoding:'utf-8'});
+
+    const base =Buffer.from(data,'base64')
+    debugger;
+
+
+    const result = decrypt(base).toString('utf-8')
+    debugger;
+    fs.writeFileSync(`${filepath.replace('.B64', '')}`, result, { encoding: 'utf-8' })
+    debugger;
+
+}
+
+/*
+const { decrypt } = require('./root/utils/nodejs/cryptos2')
+const path = require('path');
+const makeDir = require('make-dir');
+const fs = require('fs')
+const { walkSync } = require('./root/utils/nodejs/walkSync');
 debugger;
 // const data = fs.readFileSync(`${process.cwd()}/mock-data/firebaseAuthData.json`, 'utf8')
 
@@ -29,7 +65,6 @@ for (let filepath of files) {
     debugger;
 
 }
-
-
+*/
 
 
--- encryptData.js
+++ encryptData.js
@@ -24,6 +24,49 @@ for (let filepath of files) {
     const data = fs.readFileSync(filepath);
    
     const encryptedData =encrypt(data)
+
+    fs.writeFileSync(`${filepath}.B64`,encryptedData.toString('base64'),{encoding:'utf-8'})
+
+}
+
+// encrypt .env
+const envFilePath =`${process.cwd()}/.env`
+const data = fs.readFileSync(envFilePath);
+debugger;
+const encryptedData =encrypt(data)
+debugger;
+fs.writeFileSync(`${envFilePath}.B64`,encryptedData.toString('base64'),{encoding:'utf-8'})
+debugger;
+
+
+
+/*
+const { encrypt } = require('./root/utils/nodejs/cryptos2')
+const path = require('path');
+const makeDir = require('make-dir');
+const fs = require('fs')
+const { walkSync } = require('./root/utils/nodejs/walkSync');
+debugger;
+// const data = fs.readFileSync(`${process.cwd()}/mock-data/firebaseAuthData.json`, 'utf8')
+
+let files =[]
+const dirpath=`${process.cwd()}/mock-data`
+
+walkSync(dirpath, function (filepath) {
+    if (path.extname(filepath) === '.json') {
+        files.push(filepath)
+    }
+    
+
+})
+
+
+for (let filepath of files) {
+
+
+    const data = fs.readFileSync(filepath);
+   
+    const encryptedData =encrypt(data)
  
     fs.writeFileSync(`${filepath}.B64`,encryptedData,{encoding:'base64'})
 
@@ -37,3 +80,4 @@ const encryptedData =encrypt(data)
 debugger;
 fs.writeFileSync(`${envFilePath}.B64`,encryptedData,{encoding:'base64'})
 debugger;
+*/
