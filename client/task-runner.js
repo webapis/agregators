c2cf0aabbd99f7b742433e000c8db886fb497ee6
webapis <webapis.github@gmail.com>
2021-12-17 14:01:05 +0500

    fix action trigger

--- client/task-runner.js
+++ client/task-runner.js
@@ -380,3 +380,25 @@ customElements.define('runner-button', class extends HTMLElement {
     })
   }
 })
+
+
+
+
+async function triggerAction({ ticket, body, gh_action_url }) {
+  debugger;
+
+  try {
+    const response =await fetch(gh_action_url, {
+      method: 'post',
+      headers: {
+          authorization: `token ${ticket}`,
+          Accept: 'application/vnd.github.v3+json'
+      },
+      body
+  })
+  const data =await response.json()
+  } catch (error) {
+    debugger;
+  }
+ 
+}
