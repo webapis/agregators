aafbe9eadb08b12464c628aaeaf0041970e98f88
webapis <60083568+webapis@users.noreply.github.com>
2022-02-18 12:41:09 +0500

    ccv

--- features/support/hooks/initializePage.js
+++ features/support/hooks/initializePage.js
@@ -1,6 +1,7 @@
 const { httpInterceptors } = require('../puppeteer/httpsInterceptors/interceptors')
 
 async function initializePage(scenario) {
+  const { pickle: { name } } = scenario
   //1.CREATE NEW PAGE
   global.page = await global.browser.newPage()
   await global.page.setViewport({
@@ -8,9 +9,21 @@ async function initializePage(scenario) {
     height: 1250,
     deviceScaleFactor: 1,
   });
-  
+
   await httpInterceptors(scenario)
+  global.page.on('error', async (error) => {
+    await global.page.screenshot({ path: `${process.cwd()}/screenshots/${name}-${Date.now()}-error.png` });
+    console.log('page error', error)
+    throw error
+  })
+  global.page.on('pageerror', async (error) => {
+    await global.page.screenshot({ path: `${process.cwd()}/screenshots/${name}-${Date.now()}-pageerror.png` });
+    console.log('page pageerror', error)
+    throw error
+  })
   await global.page.goto('https://localhost:8888')
+
+
 
 }
 
