8e8f37819ce7f06e2295c30a41694fb907bba0be
webapis <60083568+webapis@users.noreply.github.com>
2022-02-25 11:06:09 +0500

    v

--- client/pages/input/input-configuration.js
+++ client/pages/input/input-configuration.js
@@ -33,11 +33,7 @@ customElements.define('input-configuration', class extends HTMLElement {
                 debugger;
                 this.render({ inputEditor, inputRepos: inputRepos !== null ? inputRepos : [] })
 
-            } else {
-
-                debugger;
-
-            }
+            } 
 
         })
 
@@ -45,88 +41,25 @@ customElements.define('input-configuration', class extends HTMLElement {
 
     render({ inputEditor, inputRepos }) {
         debugger;
-        const { inputName } = inputEditor
+      
         this.innerHTML = `Inputs Configuration
         <div class="row">
-        <div class="col-10">
+        <div class="col-8">
         <select id="repos" class="form-control">
         <option value="">Select Repo</option>
         </select>
         </div>
       <div class="col-2">
-      <button class="btn btn-secondary" id="add-workflow-btn">Add Repo</button>
+      <button class="btn btn-outline-secondary" id="add-workflow-btn">Add Repo</button>
       </div>
+
         </div>
-        <div class="accordion" id="accordionForRepos">
+        <div class="accordion mt-1" id="accordionForRepos">
         </div>
         `
         if(inputRepos.length>0){
             inputRepos.forEach((repoName,i)=>{
-                let open =false
-                document.getElementById('accordionForRepos').insertAdjacentHTML('beforeend',`  
-                <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${i}" aria-expanded="true" aria-controls="panelsStayOpen-collapse-${i}" id="panelsStayOpen-collapse-${i}-btn">
-                    ${repoName}
-                  </button>
-                </h2>
-                <div id="panelsStayOpen-collapse-${i}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
-                  <div class="accordion-body">
-                  <div class="row">
-                  <div class="col-10">
-                    <input type="text" class="form-control"  id="input-name-${i}"/>
-                  </div>
-                <div class="col-2">
-                <button class="btn btn-secondary" id="add-input-btn-${i}">Add Input Name</button>
-                </div>
-                  </div>
-                  </div>
-                </div>
-              </div>`)
-
-              document.getElementById(`add-input-btn-${i}`).addEventListener('click',(e)=>{
-                const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
-                const inputName=  document.getElementById(`input-name-${i}`).value
-                  window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}`).push({inputName}, (error,result) => {
-                    debugger;
-                    if (!error) {
-                      //  const inputRepos = result && Object.entries(result).map(r=>r[0])
-        
-                    
-        
-                    } else {
-        
-                        debugger;
-        
-                    }
-        
-                })
-
-              })
-              document.getElementById(`panelsStayOpen-collapse-${i}-btn`).addEventListener('click',(e)=>{
-                  e.preventDefault()
-                open = !open
-
-                if(open){
-                    console.log('open....',open)
-                    const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
-                    window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}`).get((error, result) => {
-                        debugger;
-                        if (!error) {
-                          //  const inputRepos = result && Object.entries(result).map(r=>r[0])
-            
-                        
-            
-                        } else {
-            
-                            debugger;
-            
-                        }
-            
-                    })
-                }
-
-            })//end foreach
+                insertAccordionItem(repoName,i)
 
             })
 
@@ -139,6 +72,8 @@ customElements.define('input-configuration', class extends HTMLElement {
                 const { name: inputKey } = result
                 const inputEditor = JSON.parse(localStorage.getItem('inputEditor'))
                 localStorage.setItem('inputEditor', JSON.stringify({ ...inputEditor, repoName: '', inputName: '', inputs: [...inputEditor.inputs, { inputName, inputKey, repoName }] }))
+                insertAccordionItem(repoName,inputKey)
+//----------------------------------
             })
         })
 
@@ -162,90 +97,139 @@ customElements.define('input-configuration', class extends HTMLElement {
             localStorage.setItem('inputEditor', JSON.stringify({ ...inputEditor, repoName: value }))
         })
 
-        // document.getElementById(`update-input_btn`).addEventListener('click', (e) => {
-        //     const { inputName, repoName, inputKey } = JSON.parse(localStorage.getItem('inputEditor'))
-        //     const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
-        //     window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}/${inputKey}`).update({ inputName }, (error, result) => {
-        //         const inputEditor = JSON.parse(localStorage.getItem('inputEditor'))
-        //         localStorage.setItem('inputEditor', JSON.stringify({ ...inputEditor, repoName: '', inputName: '', inputKey: '', editVar: false }))
-        //         location.reload()
-        //     })
-        // })
-
-        if (false) {
-            workflows.forEach(workflow => {
-                debugger;
-                const repoName = workflow[0]
-                const inputs = workflow[1]
-                for (let inputKey in inputs) {
-                    const inputName = inputs[inputKey]['inputName']
-                    document.getElementById('var-table').insertAdjacentHTML('beforeend', `<tr id ="${inputKey}-tr">
-                    <th scope="row"></th>
-                    <td>${repoName}</td>
-                    <td>${inputName}</td>
-                    <td><button class="btn btn-outline-secondary" id="${inputKey}-edit-btn">Edit</button></td>
-                    <td><button class="btn btn-outline-secondary" id="${inputKey}-remove-btn">Remove</button></td>
-                  </tr>`)
-
-                    document.getElementById(`${inputKey}-edit-btn`).addEventListener('click', (e) => {
-                        e.preventDefault()
-                        debugger;
-                        const inputEditor = JSON.parse(localStorage.getItem('inputEditor'))
-                        localStorage.setItem('inputEditor', JSON.stringify({ ...inputEditor, repoName, inputName, inputKey, editVar: true }))
-
-                        document.getElementById('repos').innerHTML = ''
-                        document.getElementById('repos').insertAdjacentHTML('afterbegin', `<option value="${repoName}" selected>${repoName}</option>`)
-                        document.getElementById('repos').setAttribute('disabled', true)
-                        document.getElementById('input-name').value = inputName
-                        document.getElementById('update-input_btn').removeAttribute('disabled')
-                        debugger;
-
-                    })
-
-                    document.getElementById(`${inputKey}-remove-btn`).addEventListener('click', (e) => {
-                        const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
-                        window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}/${inputKey}`).remove((error, result) => {
-                            location.reload()
-                        })
-                    })
-
-                }
-
-            })
-        }
-
-        // document.getElementById('input-name').addEventListener('input', (e) => {
-        //     const { value } = e.target
-
-
-        //     const inputEditor = JSON.parse(localStorage.getItem('inputEditor'))
-        //     localStorage.setItem('inputEditor', JSON.stringify({ ...inputEditor, inputName: value }))
-        // })
-
-
-
-        // document.getElementById('add-input-btn').addEventListener('click', (e) => {
-        //     const { inputName, repoName } = JSON.parse(localStorage.getItem('inputEditor'))
-        //     const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
-
-        //     window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}`).push({ inputName }, (error, result) => {
-        //         const { name: inputKey } = result
-        //         const inputEditor = JSON.parse(localStorage.getItem('inputEditor'))
-        //         localStorage.setItem('inputEditor', JSON.stringify({ ...inputEditor, repoName: '', inputName: '', inputs: [...inputEditor.inputs, { inputName, inputKey, repoName }] }))
-        //         document.getElementById('var-table').insertAdjacentHTML('beforeend', `<tr id ="${repoName}-tr">
-        //         <th scope="row"></th>
-        //         <td>${repoName}</td>
-        //         <td>${inputName}</td>
-  
-        //         <td><button class="btn btn-outline-secondary">Edit</button></td>
-        //         <td><button class="btn btn-outline-secondary">Remove</button></td>
-        //       </tr>`)
-
-        //     })
-        // })
 
     }
 })
 
 
 
+function removeInput(repoName,inputKey){
+    const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
+    window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}/${inputKey}`).remove((error, result) => {
+        const parent =document.getElementById(inputKey).parentNode
+        parent.removeChild(document.getElementById(inputKey))
+    })
+}
+
+
+function insertAccordionItem(repoName,i){
+    let open =false
+    document.getElementById('accordionForRepos').insertAdjacentHTML('beforeend',`  
+    <div class="accordion-item" id="${i}-accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${i}" aria-expanded="true" aria-controls="panelsStayOpen-collapse-${i}" id="panelsStayOpen-collapse-${i}-btn">
+        ${repoName}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapse-${i}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+      <div class="accordion-body">
+
+      <div class="row">
+
+      <div class="col">
+        <input type="text" class="form-control"  id="input-name-${i}"/>
+      </div>
+
+      <div class="col">
+      <button class="btn btn-outline-secondary" id="add-input-btn-${i}">Add Input</button>
+      </div>
+      <div class="col d-flex justify-content-end m-0">
+      <button class="btn btn-outline-danger"  id="remove-all-input-btn-${i}">Remove All</button>
+      </div>
+      </div>
+        <div class="row mt-1">
+        
+        <div class="col">
+        <ul class="list-group" id="inputlist-${i}">
+        </ul>
+        </div>
+        
+        </div>
+
+      </div>
+    </div>
+  </div>`)
+debugger;
+document.getElementById(`remove-all-input-btn-${i}`).addEventListener('click',(e)=>{
+    const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
+    const inputName =  document.getElementById(`input-name-${i}`).value
+    debugger;
+    const updateInputs = { [`inputs/workspaces/${workspaceName}/repos/${repoName}`]: null }
+    const updateWorkflows = { [`inputRepos/workspaces/${workspaceName}/repos/${repoName}`]: null }
+      window.FB_DATABASE.ref('/').update({...updateInputs,...updateWorkflows}, (error,result) => {
+        debugger;
+        if (!error) {   
+    
+            const parent =document.getElementById(`${i}-accordion-item`).parentNode
+            parent.removeChild(document.getElementById(`${i}-accordion-item`))
+           
+           
+        } else {
+
+            debugger;
+
+        }
+
+    })// 
+  })
+
+  document.getElementById(`add-input-btn-${i}`).addEventListener('click',(e)=>{
+    const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
+    const inputName =  document.getElementById(`input-name-${i}`).value
+    debugger;
+      window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}`).push({inputName}, (error,result) => {
+        debugger;
+        if (!error) {   
+         const inputKey =result['name']
+
+                debugger;
+                document.getElementById(`inputlist-${i}`).insertAdjacentHTML('beforeend',` <li class="list-group-item d-flex justify-content-between align-items-center" id="${inputKey}">
+                ${inputName}
+                <button class="btn btn-outline-danger btn-sm" id="${inputKey}-btn">Remove</button>
+              </li>`)
+
+              document.getElementById(`${inputKey}-btn`).addEventListener('click',(e)=>{
+                removeInput(repoName,inputKey)
+              })
+        } else {
+
+            debugger;
+
+        }
+
+    })//
+
+  })
+  document.getElementById(`panelsStayOpen-collapse-${i}-btn`).addEventListener('click',(e)=>{
+      e.preventDefault()
+    open = !open
+
+    if(open){
+        console.log('open....',open)
+        const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
+        window.FB_DATABASE.ref(`inputs/workspaces/${workspaceName}/repos/${repoName}`).get((error, inputs) => {
+            debugger;
+            if (!error) {
+           
+              document.getElementById(`inputlist-${i}`).innerHTML=''
+              for(let inputKey in inputs){
+                const inputName =inputs[inputKey]['inputName']
+               
+                document.getElementById(`inputlist-${i}`).insertAdjacentHTML('beforeend',`  <li class="list-group-item d-flex justify-content-between align-items-center" id="${inputKey}">
+                ${inputName}
+                <button class="btn btn-outline-danger btn-sm" id="${inputKey}-btn">Remove</button>
+              </li>`)
+
+               document.getElementById(`${inputKey}-btn`).addEventListener('click',(e)=>{
+                removeInput(repoName,inputKey)
+              })
+            }
+        
+            } 
+
+        })
+    }
+
+})//end foreach
+
+}
