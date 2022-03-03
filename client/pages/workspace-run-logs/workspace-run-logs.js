b8bf3edacefc933cd424b1978af2eaeb7e70a58e
webapis <60083568+webapis@users.noreply.github.com>
2022-03-03 15:15:30 +0500

    v

--- client/pages/workspace-run-logs/workspace-run-logs.js
+++ client/pages/workspace-run-logs/workspace-run-logs.js
@@ -41,21 +41,30 @@ customElements.define('workspace-accordion-item', class extends HTMLElement {
 
     connectedCallback() {
         const dataId = this.getAttribute('data-id')
-        const last = new Date(parseInt(this.getAttribute('last'))).toDateString()
-        const start = new Date(parseInt(this.getAttribute('start'))).toDateString()
+        const last = new Date(parseInt(this.getAttribute('last')))
+        const start = new Date(parseInt(this.getAttribute('start')))
         const success = this.getAttribute('success')
         const failed = this.getAttribute('failed')
         const totalTasks = this.getAttribute('totalTasks')
         const totalWorkflows = this.getAttribute('totalWorkflows')
         const runid = this.getAttribute('runid')
+        const { hours, mins, seconds } = timespan(new Date(parseInt(this.getAttribute('last'))), new Date(parseInt(this.getAttribute('start'))))
+        const duration = `${hours}:${mins}:${seconds}`
+
+        const startTime = `${start.getHours()}:${start.getMinutes()}:${start.getSeconds()}`
+        const endTime = `${last.getHours()}:${last.getMinutes()}:${last.getSeconds()}`
+        const date =`${start.getDate()}:${start.getMonth()+1}:${start.getFullYear()}`
+        debugger;
         this.open = false
 
         this.innerHTML = `
         <div class="accordion-item">
         <h2 class="accordion-header" id="heading-${dataId}">
           <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${dataId}" aria-expanded="true" aria-controls="collapse-${dataId}" id="ws-${dataId}-btn">
-          <span class="badge bg-info text-dark fw-normal">start: <span class="badge bg-secondary fw-normal">${start}</span></span>
-          <span class="badge bg-info text-dark m-1 fw-normal">last: <span class="badge bg-secondary fw-normal">${last}</span></span>
+          <span class="badge bg-info text-dark fw-normal m-1">Date: <span class="badge bg-secondary fw-normal">${date}</span></span>
+          <span class="badge bg-info text-dark fw-normal">start: <span class="badge bg-secondary fw-normal">${startTime}</span></span>
+          <span class="badge bg-info text-dark m-1 fw-normal">last: <span class="badge bg-secondary fw-normal">${endTime}</span></span>
+          <span class="badge bg-info text-dark m-1 fw-normal">Duration: <span class="badge bg-secondary fw-normal">${duration}</span></span>
           <span class="badge bg-info text-dark m-1 fw-normal">totalTasks: <span class="badge bg-secondary fw-normal">${totalTasks}</span></span>
           <span class="badge bg-info text-dark m-1 fw-normal">totalWorkflows: <span class="badge bg-secondary fw-normal">${totalWorkflows}</span></span>
           <span class="badge bg-info text-dark m-1 fw-normal">success: <span class="badge bg-secondary fw-normal">${success}</span></span>
@@ -93,16 +102,16 @@ customElements.define('workspace-accordion-item', class extends HTMLElement {
 
                             const task = await getResponse.json()
 
-                            return { ...task,taskId,runid, end, start, failed, success, total }
+                            return { ...task, taskId, runid, end, start, failed, success, total }
 
                         })())
-                   
+
                     }//end for loop
                     const tasks = await Promise.all(taskPromises)
-                
-                  //  document.getElementById(`body-${runid}`).innerHTML=''
-                    tasks.forEach((task,i) => {
-                  
+
+                    //  document.getElementById(`body-${runid}`).innerHTML=''
+                    tasks.forEach((task, i) => {
+
                         const end = task['end']
                         const failed = task['failed']
                         const runOrder = task['runOrder']
@@ -111,16 +120,16 @@ customElements.define('workspace-accordion-item', class extends HTMLElement {
                         const success = task['success']
                         const taskName = task['taskName']
                         const total = task['total']
-                        const taskId=task['taskId']
-                        const runid =this.getAttribute('runid')
-                      
+                        const taskId = task['taskId']
+                        const runid = this.getAttribute('runid')
+
                         document.getElementById(`body-${runid}`).insertAdjacentHTML('beforeend', `
                         <task-accordion-item taskName="${taskName}" runid="${runid}" taskId="${taskId}" runOrder="${runOrder}" runSequence="${runSequence}" total="${total}" success="${success}" failed="${failed}" start="${start}" end="${end}" data-id="${i}">
                         </task-accordion-item>`)
                     })
                 })
-            } else{
-               // document.getElementById(`body-${runid}`).innerHTML=''
+            } else {
+                // document.getElementById(`body-${runid}`).innerHTML=''
             }
 
         })
@@ -135,91 +144,98 @@ customElements.define('task-accordion-item', class extends HTMLElement {
     }
 
     connectedCallback() {
-        this.open=false
+        this.open = false
         const dataId = this.getAttribute('data-id')
-        const taskName =this.getAttribute('taskName')
-        const runOrder=this.getAttribute('runOrder')
-        const runSequence=this.getAttribute('runSequence')
-        const start =this.getAttribute('start')
-        const end =this.getAttribute('end')
-        const success=this.getAttribute('success')
-        const failed =this.getAttribute('failed')
-        const total= this.getAttribute('total')
-        const taskId=this.getAttribute('taskId')
-        const runid =this.getAttribute('runid')
+        const taskName = this.getAttribute('taskName')
+        const runOrder = this.getAttribute('runOrder')
+        const runSequence = this.getAttribute('runSequence')
+        const start =new Date(parseInt( this.getAttribute('start')))
+        const end = new Date(parseInt( this.getAttribute('end')))
+        const success = this.getAttribute('success')
+        const failed = this.getAttribute('failed')
+        const total = this.getAttribute('total')
+        const taskId = this.getAttribute('taskId')
+        const runid = this.getAttribute('runid')
+        const { hours, mins, seconds } = timespan(end, start)
+        const duration = `${hours}:${mins}:${seconds}`
+        const startTime = `${start.getHours()}:${start.getMinutes()}:${start.getSeconds()}`
+        const endTime = `${end.getHours()}:${end.getMinutes()}:${end.getSeconds()}`
+    
         debugger;
         this.innerHTML = `
         <div class="accordion-item">
-        <h2 class="accordion-header" id="heading-${dataId}">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${dataId}" aria-expanded="true" aria-controls="collapse-${dataId}" id="${dataId}-wf-btn">
-          <span class="badge  text-dark m-1 fw-light">TaskName: <span class="badge bg-secondary fw-normal">${taskName}</span></span>
+        <h2 class="accordion-header" id="heading-${taskId}-${runid}">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${taskId}-${runid}" aria-expanded="true" aria-controls="collapse-${taskId}-${runid}" id="${taskId}-${runid}-wf-btn">
+        
+          <span class="badge  text-dark m-1 fw-light">TaskName:  <span class="badge bg-secondary fw-normal">${taskName}</span></span>
           <span class="badge  text-dark m-1 fw-light">Run Order: <span class="badge bg-secondary fw-normal">${runOrder}</span></span>
-          <span class="badge  text-dark m-1 fw-light">Run Order: <span class="badge bg-secondary fw-normal">${runSequence}</span></span>
-          <span class="badge  text-dark m-1 fw-light">Total: <span class="badge bg-secondary fw-normal">${total}</span></span>
-          <span class="badge  text-dark m-1 fw-light">Start: <span class="badge bg-secondary fw-normal">${start}</span></span>
-          <span class="badge  text-dark m-1 fw-light">End: <span class="badge bg-secondary fw-normal">${end}</span></span>
-          <span class="badge  text-dark m-1 fw-light">Success: <span class="badge bg-secondary fw-normal">${success}</span></span>
-          <span class="badge  text-dark m-1 fw-light">Failed: <span class="badge bg-secondary fw-normal">${failed}</span></span>
+          <span class="badge  text-dark m-1 fw-light">Total:     <span class="badge bg-secondary fw-normal">${total}</span></span>
+          <span class="badge  text-dark m-1 fw-light">Start:     <span class="badge bg-secondary fw-normal">${startTime}</span></span>
+          <span class="badge  text-dark m-1 fw-light">End:       <span class="badge bg-secondary fw-normal">${endTime}</span></span>
+          <span class="badge  text-dark m-1 fw-light">Duration:       <span class="badge bg-secondary fw-normal">${duration}</span></span>
+          <span class="badge  text-dark m-1 fw-light">Success:   <span class="badge bg-secondary fw-normal">${success}</span></span>
+          <span class="badge  text-dark m-1 fw-light">Failed:    <span class="badge bg-secondary fw-normal">${failed}</span></span>
+          <span class="badge  text-dark m-1 fw-light">Run Sequence: <span class="badge bg-secondary fw-normal">${runSequence}</span></span>
           </button>
         </h2>
-        <div id="collapse-${dataId}" class="accordion-collapse collapse" aria-labelledby="heading-${dataId}" data-bs-parent="#accordionExample">
-          <div class="accordion-body" id="body-wf-${taskId}">
+        <div id="collapse-${taskId}-${runid}" class="accordion-collapse collapse" aria-labelledby="heading-${taskId}-${runid}" data-bs-parent="#accordionExample">
+          <div class="accordion-body" id="body-wf-${taskId}-${runid}">
              
           </div>
         </div>
       </div>`
 
-      document.getElementById(`${dataId}-wf-btn`).addEventListener('click',(e)=>{
-          this.open=!this.open
-          if(this.open){
-            const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
-            const { idToken } = JSON.parse(localStorage.getItem('auth'))
-            window.FB_DATABASE.ref(`/workflowLogs/${workspaceName}/${runid}/tasks/${taskId}/workflows`).on('value', async (error, { data }) => {
-           
-                let workPromises = []
-                for (let workflowKey in data) {
+        document.getElementById(`${taskId}-${runid}-wf-btn`).addEventListener('click', (e) => {
+            this.open = !this.open
+            if (this.open) {
+                const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
+                const { idToken } = JSON.parse(localStorage.getItem('auth'))
+                window.FB_DATABASE.ref(`/workflowLogs/${workspaceName}/${runid}/tasks/${taskId}/workflows`).on('value', async (error, { data }) => {
 
-                    const end = data[workflowKey]['log']['end']
-                    const start = data[workflowKey]['log']['start']
-                    const result = data[workflowKey]['log']['result']
-             
-              
+                    let workPromises = []
+                    for (let workflowKey in data) {
 
-                    workPromises.push((async () => {
-                        const fetchUrl = `${window.projectUrl}/workflows/workspaces/${workspaceName}/tasks/${taskId}/${workflowKey}/.json?auth=${idToken}`
-                 
-                        const getResponse = await fetch(fetchUrl, { method: 'GET' })
+                        const end = data[workflowKey]['log']['end']
+                        const start = data[workflowKey]['log']['start']
+                        const result = data[workflowKey]['log']['result']
 
-                        const workflow = await getResponse.json()
 
-                        return { ...workflow,taskId,runid, end, start, result }
 
-                    })())
-               
-                }//end for loop
-                const workflows = await Promise.all(workPromises)
-                workflows.forEach(wf=>{
-                    debugger;
-                            const end =wf['end']
-                            const repoName=wf["repoName"]
-                            const result=wf["result"]
-                            const runid =wf['runid']
-                            const screenName=wf["screenName"]
-                            const selectedBranch=wf["selectedBranch"]
-                            const start=wf['start']
-                            const workflowDescription=wf['workflowDescription']
+                        workPromises.push((async () => {
+                            const fetchUrl = `${window.projectUrl}/workflows/workspaces/${workspaceName}/tasks/${taskId}/${workflowKey}/.json?auth=${idToken}`
 
-                           document.getElementById(`body-wf-${taskId}`).insertAdjacentHTML('beforeend', 
-                `<workflow-list-items repoName="${repoName}" selectedBranch="${selectedBranch}" owner="${screenName}" start="${start}" end="${end}" result="${result}" workflowDescription="${workflowDescription}">
+                            const getResponse = await fetch(fetchUrl, { method: 'GET' })
+
+                            const workflow = await getResponse.json()
+
+                            return { ...workflow, taskId, runid, end, start, result }
+
+                        })())
+
+                    }//end for loop
+                    const workflows = await Promise.all(workPromises)
+                    workflows.forEach(wf => {
+                        debugger;
+                        const end = wf['end']
+                        const repoName = wf["repoName"]
+                        const result = wf["result"]
+                        const runid = wf['runid']
+                        const screenName = wf["screenName"]
+                        const selectedBranch = wf["selectedBranch"]
+                        const start = wf['start']
+                        const workflowDescription = wf['workflowDescription']
+
+                        document.getElementById(`body-wf-${taskId}-${runid}`).insertAdjacentHTML('beforeend',
+                            `<workflow-list-items repoName="${repoName}" selectedBranch="${selectedBranch}" owner="${screenName}" start="${start}" end="${end}" result="${result}" workflowDescription="${workflowDescription}">
                  </workflow-list-items>`)
-                    debugger;
-                })
+                        debugger;
+                    })
 
-            
-            })
-          }
-         
-      })
+
+                })
+            }
+
+        })
     }
 })
 
@@ -231,23 +247,28 @@ customElements.define('workflow-list-items', class extends HTMLElement {
 
     connectedCallback() {
         const dataId = this.getAttribute('data-id')
-        const taskId=this.getAttribute
-        const repoName=this.getAttribute('repoName')
-        const selectedBranch=this.getAttribute('selectedBranch')
-        const owner=this.getAttribute('owner')
-        const start=this.getAttribute('start')
-        const end=this.getAttribute('end')
-        const result =this.getAttribute('result')
-        const desc= this.getAttribute('workflowDescription')
-
+        const taskId = this.getAttribute
+        const repoName = this.getAttribute('repoName')
+        const selectedBranch = this.getAttribute('selectedBranch')
+        const owner = this.getAttribute('owner')
+        const start =new Date(parseInt( this.getAttribute('start')))
+        const end = new Date(parseInt( this.getAttribute('end')))
+        const result = this.getAttribute('result')
+        const desc = this.getAttribute('workflowDescription')
+        const { hours, mins, seconds } = timespan(end, start)
+        const duration = `${hours}:${mins}:${seconds}`
+        const startTime = `${start.getHours()}:${start.getMinutes()}:${start.getSeconds()}`
+        const endTime = `${end.getHours()}:${end.getMinutes()}:${end.getSeconds()}`
         debugger;
-        this.innerHTML =`
+        this.innerHTML = `
         <div>
+        <span class="badge  text-dark m-1 fw-normal">Workflow Name: <span class="badge bg-secondary fw-normal" >${desc}</span></span>
         <span class="badge  text-dark m-1 fw-normal">RepoName: <span class="badge bg-secondary fw-normal" >${repoName}</span></span>
         <span class="badge  text-dark m-1 fw-normal">Selected Branch: <span class="badge bg-secondary fw-normal" >${selectedBranch}</span></span>
         <span class="badge  text-dark m-1 fw-normal">Owner: <span class="badge bg-secondary fw-normal" >${owner}</span></span>
-        <span class="badge  text-dark m-1 fw-normal">Start: <span class="badge bg-secondary fw-normal" >${start}</span></span>
-        <span class="badge  text-dark m-1 fw-normal">End: <span class="badge bg-secondary fw-normal" >${end}</span></span>
+        <span class="badge  text-dark m-1 fw-normal">Start: <span class="badge bg-secondary fw-normal" >${startTime}</span></span>
+        <span class="badge  text-dark m-1 fw-normal">End: <span class="badge bg-secondary fw-normal" >${endTime}</span></span>
+        <span class="badge  text-dark m-1 fw-normal">Duration: <span class="badge bg-secondary fw-normal" >${duration}</span></span>
         <span class="badge  text-dark m-1 fw-normal">Result: <span class="badge bg-secondary fw-normal" >${result}</span></span>
         <span class="badge  text-dark m-1 fw-normal">Desc: <span class="badge bg-secondary fw-normal" >${desc}</span></span>
       </div>`
@@ -258,3 +279,23 @@ customElements.define('workflow-list-items', class extends HTMLElement {
 
 
 
+function timespan(date2, date1) {
+
+
+    var diff = date2.getTime() - date1.getTime();
+
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    diff -= days * (1000 * 60 * 60 * 24);
+
+    var hours = Math.floor(diff / (1000 * 60 * 60));
+    diff -= hours * (1000 * 60 * 60);
+
+    var mins = Math.floor(diff / (1000 * 60));
+    diff -= mins * (1000 * 60);
+
+    var seconds = Math.floor(diff / (1000));
+    diff -= seconds * (1000);
+
+    console.log(days + " days, " + hours + " hours, " + mins + " minutes, " + seconds + " seconds");
+    return { days, hours, mins, seconds }
+}
