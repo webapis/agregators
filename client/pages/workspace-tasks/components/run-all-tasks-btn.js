f6d5fe0831b50f627c8cea4e80b86c4d6765123a
webapis <60083568+webapis@users.noreply.github.com>
2022-03-16 09:38:18 +0500

    1.12.42

--- client/pages/workspace-tasks/components/run-all-tasks-btn.js
+++ client/pages/workspace-tasks/components/run-all-tasks-btn.js
@@ -7,8 +7,8 @@ customElements.define('run-all-tasks-btn', class extends HTMLElement {
         this.innerHTML = ` <button class="btn btn-outline-success btn-sm" id="run-all-tasks-btn">Run </button>`
         document.getElementById(`run-all-tasks-btn`).addEventListener('click', async (e) => {
 
-         //  document.getElementById('run-all-tasks-btn').setAttribute('disabled',true)
-           
+            //  document.getElementById('run-all-tasks-btn').setAttribute('disabled',true)
+
             const { title: workspaceName } = JSON.parse(localStorage.getItem('workspace'))
             const { idToken, localId: uid, token, screenName: owner, email, refreshToken } = JSON.parse(localStorage.getItem('auth'))
             const runid = Date.now()
@@ -17,11 +17,11 @@ customElements.define('run-all-tasks-btn', class extends HTMLElement {
             const taskId = window.orderedTasks[0]['taskId']
             const runSequence = window.orderedTasks[0]['runSequence']
             const first = 'true'
-            
+
             const fetchIncUrl = `${window.projectUrl}/inc/workspaces/${workspaceName}/inc/.json?auth=${idToken}`
-            const updateIncResponse = await fetch(fetchIncUrl, { method: 'PUT', body: JSON.stringify({ '.sv': {"increment":1}  })})
-            const incData =await updateIncResponse.json()
-        
+            const updateIncResponse = await fetch(fetchIncUrl, { method: 'PUT', body: JSON.stringify({ '.sv': { "increment": 1 } }) })
+            const incData = await updateIncResponse.json()
+
             const wfrunid = incData
             const parameters = `${token}--xxx--${owner}--xxx--${idToken}--xxx--${email}--xxx--${uid}--xxx--${refreshToken}--xxx--${'selectedContainer'}--xxx--${window.projectUrl}--xxx--${workspaceName}--xxx--${runid}--xxx--${start}--xxx--${taskId}--xxx--${runNext}--xxx--${runSequence}--xxx--${first}--xxx--${wfrunid}`
             const body = JSON.stringify({ ref: 'main', inputs: { projectName: workspaceName, parameters } })
@@ -29,17 +29,18 @@ customElements.define('run-all-tasks-btn', class extends HTMLElement {
             const updateWsLastLogStart = { start: { '.sv': 'timestamp' }, last: null }
 
             const updateLastLogResponse = await fetch(fetchUrl, { method: 'put', body: JSON.stringify(updateWsLastLogStart) })
-         
+
             if (workspaceName === 'local_ws_bdd' || workspaceName === 'local_pub_ws_bdd') {
                 const response = await fetch('http://localhost:3001', { body, method: 'post' })
-            } else
-            {
-                await triggerAction({ gh_action_url: `https://api.github.com/repos/${owner}/workflow_runner/actions/workflows/aggregate.yml/dispatches`, ticket: token, body })
+            } else {
+                const response = await triggerAction({ gh_action_url: `https://api.github.com/repos/${owner}/workflow_runner/actions/workflows/aggregate.yml/dispatches`, ticket: token, body })
+                const data = await response.json()
+                debugger;
             }
 
         })
     }
- 
+
 })
 
 async function triggerAction({ ticket, body, gh_action_url }) {
