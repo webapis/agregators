0fbea2c7da7b2c242fd5718d5326bcbcc7a52b03
webapis <60083568+webapis@users.noreply.github.com>
2022-03-24 08:48:43 +0500

    vvvv

--- netlify/functions/google-refresh/google-refresh.js
+++ netlify/functions/google-refresh/google-refresh.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const { nodeFetch } = require('../../../root/utils/nodejs/node-fetch')
 const client_id = process.env.client_id
+console.log('client_id',client_id)
 const client_secret = process.env.CLIENT_SECRET
+console.log('client_secret',client_secret)
 exports.handler = async function (event, context) {
     const { refresh_token } = event.queryStringParameters
+    console.log('refresh_token',refresh_token)
     debugger;
     const response = await nodeFetch({ host: 'oauth2.googleapis.com', path: `/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=refresh_token&refresh_token=${refresh_token}`, method: 'post', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json' } })
    debugger;
